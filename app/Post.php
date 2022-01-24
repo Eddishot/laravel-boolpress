4250c7522d81fa6ef49477fd1346ada1076940b3
Eddishot <Eddi_sl94@hotmail.it>
2022-01-24 01:27:01 +0100

    relation between Model Post and User

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ["name", "title", "content"];
+
+
+    public function postsUser(){
+        return $this->belongsto("App\User");
+    }
 }
